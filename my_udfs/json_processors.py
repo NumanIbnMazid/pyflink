89a32a740e3d35c9ad64a60803d06f244cece637
sunny-selise <raihan.sunny@selise.ch>
2022-10-24 18:14:16 +0600

    Demo topology with stream splitting and joining

---
+++ my_udfs/json_processors.py
@@ -0,0 +1,28 @@
+from time import sleep
+import json
+import logging
+
+
+def add_count(data):
+    # parse the json
+    json_data = json.loads(data[1])
+    json_data['count'] += 1
+    logging.info(f"Processing record {data[0]} for 2 seconds...")
+    print(f"Processing record {data[0]} for 2 seconds...")
+    sleep(2)
+    return data[0], json_data
+
+
+def filter_by_country(data):
+    # the json data could be accessed directly, no need to parse it again using json.loads
+    return "China" in data[1]['addr']['country']
+
+
+def verify(data):
+    data[1].update(verified=True)
+    return data
+
+
+def modify(data):
+    data[1].update(published=True)
+    return data
--- process_json.py
+++ process_json.py
@@ -1,77 +1,90 @@
-from time import sleep
-import json
+import os
+import copy
 import logging
 import sys
+import json
 
-from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.common.serialization import Encoder
+from pyflink.common.typeinfo import Types
+from pyflink.datastream import StreamExecutionEnvironment, CoMapFunction
+from pyflink.datastream.functions import RuntimeContext
+#from pyflink.datastream.state import ValueStateDescriptor
+from pyflink.datastream.connectors import (FileSource, StreamFormat, FileSink, OutputFileConfig,
+                                           RollingPolicy)
 
+from json_processors import add_count, verify, modify
+
+
+class JoinResultFunction(CoMapFunction):
+    def __init__(self):
+        self.state = {}
+
+    #def open(self, runtime_context: RuntimeContext):
+    #    self.state = runtime_context.get_state(ValueStateDescriptor(
+    #        "my_state", Types.PICKLED_BYTE_ARRAY()))
+
+    def map1(self, value):
+        result = None
+        if value[0] not in self.state:
+            self.state.update({value[0]: value[1]})
+        else:
+            res = copy.deepcopy(value[1])
+            res.update(self.state[value[0]])
+            result = (value[0], json.dumps(res))
+            self.state.pop(value[0])
+        return result
+
+    def map2(self, value):
+        result = None
+        if value[0] not in self.state:
+            self.state.update({value[0]: value[1]})
+        else:
+            res = copy.deepcopy(value[1])
+            res.update(self.state[value[0]])
+            result = (value[0], json.dumps(res))
+            self.state.pop(value[0])
+        return result
+    
 
 def process_json_data():
     env = StreamExecutionEnvironment.get_execution_environment()
+    env.set_parallelism(1)
+    env.disable_operator_chaining()
+
+    # add python dependencies
+    dep = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'my_udfs')
+    env.add_python_file(dep)
 
     # define the source
     ds = env.from_collection(
         collection=[
-            (1, '{"name": "Flink", "tel": 123, "addr": {"country": "Germany", "city": "Berlin"}}'),
-            (2, '{"name": "hello", "tel": 135, "addr": {"country": "China", "city": "Shanghai"}}'),
-            (3, '{"name": "world", "tel": 124, "addr": {"country": "USA", "city": "NewYork"}}'),
-            (4, '{"name": "PyFlink", "tel": 32, "addr": {"country": "China", "city": "Hangzhou"}}')]
-    )
+            (1, '{"name": "Flink", "count": 123, "addr": {"country": "Germany", "city": "Berlin"}}'),
+            (2, '{"name": "hello", "count": 135, "addr": {"country": "China", "city": "Shanghai"}}'),
+            (3, '{"name": "world", "count": 124, "addr": {"country": "USA", "city": "NewYork"}}'),
+            (4, '{"name": "PyFlink", "count": 32, "addr": {"country": "China", "city": "Hangzhou"}}')]
+    ).name("JSON Data Source")
 
-    def add_tel(data):
-        # parse the json
-        json_data = json.loads(data[1])
-        json_data['tel'] += 1
-        logging.info("Going to sleep for 5 seconds...")
-        sleep(5)
-        return data[0], json_data
+    # define operations
+    ds = ds.map(add_count).name("add_1_to_count")
+    verified_ds = ds.map(verify).name("verify_data")
+    modified_ds = ds.map(modify).name("modify_data")
+    final_ds = verified_ds.connect(modified_ds).map(
+        JoinResultFunction(), output_type=Types.TUPLE([Types.INT(), Types.STRING()])).name("join_data")
 
-    def sub_tel(data):
-        data[1]['tel'] -= 2
-        return data
-
-    def filter_by_country(data):
-        # the json data could be accessed directly, there is no need to parse it again using
-        # json.loads
-        return "China" in data[1]['addr']['country']
-
-    ds.map(add_tel).filter(filter_by_country).start_new_chain().map(sub_tel).print()
+    # create sink and emit result to sink
+    output_path = '/tmp/output/'
+    file_sink = FileSink \
+        .for_row_format(output_path, Encoder.simple_string_encoder()) \
+        .with_output_file_config(OutputFileConfig.builder().with_part_prefix('pre').with_part_suffix('suf').build()) \
+        .build()
+    final_ds.sink_to(file_sink).name("File Sink")
+    #final_ds.print()
 
     # submit for execution
-    env.execute_async("Processing Job")
+    env.execute("Process JSON Data")
 
-def reprocess_json_data():
-    env = StreamExecutionEnvironment.get_execution_environment()
-
-    # define the source
-    ds = env.from_collection(
-        collection=[
-            (5, '{"name": "Flink", "tel": 123, "addr": {"country": "Germany", "city": "Berlin"}}'),
-            (6, '{"name": "hello", "tel": 135, "addr": {"country": "China", "city": "Shanghai"}}'),
-            (7, '{"name": "world", "tel": 124, "addr": {"country": "USA", "city": "NewYork"}}'),
-            (8, '{"name": "PyFlink", "tel": 32, "addr": {"country": "China", "city": "Hangzhou"}}')]
-    )
-
-    def add_tel(data):
-        # parse the json
-        json_data = json.loads(data[1])
-        json_data['tel'] += 1
-        logging.info("Going to sleep for 4 seconds...")
-        sleep(4)
-        return data[0], json_data
-
-    def filter_by_country(data):
-        # the json data could be accessed directly, there is no need to parse it again using
-        # json.loads
-        return "Germany" in data[1]['addr']['country']
-
-    ds.map(add_tel).filter(filter_by_country).print()
-
-    # submit for execution
-    env.execute_async("Reprocessing Job")
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
 
     process_json_data()
-    reprocess_json_data()
