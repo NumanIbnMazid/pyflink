7adfc91deb3a48928d95ed3ece342f881411e091
sunny-selise <raihan.sunny@selise.ch>
2022-11-30 15:54:47 +0600

    Demo data modified to monotonous timestamps

--- session_window.py
+++ session_window.py
@@ -20,17 +20,17 @@ class MyTimestampAssigner(TimestampAssigner):
 
 
 class UserSessionFunction(AggregateFunction):
-    def create_accumulator(self):
+    def create_accumulator(self) -> UserActivity:
         return UserActivity()
 
-    def add(self, value, user_activity: UserActivity):
+    def add(self, value, user_activity: UserActivity) -> UserActivity:
         user_activity.user = value["user"]
         user_activity.activity_start = min(value["timestamp"], user_activity.activity_start)
         user_activity.activity_end = max(value["timestamp"], user_activity.activity_end)
         user_activity.num_interactions += 1
         return user_activity
 
-    def get_result(self, user_activity: UserActivity):
+    def get_result(self, user_activity: UserActivity) -> UserActivity:
         return user_activity
 
     def merge(self, acc_a, acc_b):
@@ -46,12 +46,13 @@ if __name__ == '__main__':
         {"id": 1, "user": "sharif", "timestamp": 1},
         {"id": 2, "user": "numan", "timestamp": 1},
         {"id": 3, "user": "hasan", "timestamp": 2},
-        {"id": 4, "user": "numan", "timestamp": 6},
+        {"id": 4, "user": "numan", "timestamp": 2},
         {"id": 5, "user": "sunny", "timestamp": 3},
         {"id": 6, "user": "sunny", "timestamp": 4},
-        {"id": 7, "user": "numan", "timestamp": 5},
-        {"id": 8, "user": "sunny", "timestamp": 7},
-        {"id": 9, "user": "sunny", "timestamp": 6},
+        {"id": 7, "user": "numan", "timestamp": 4},
+        {"id": 8, "user": "sunny", "timestamp": 6},
+        {"id": 9, "user": "sunny", "timestamp": 9},
+        {"id": 10, "user": "numan", "timestamp": 9},
     ])
 
     # define the watermark strategy
