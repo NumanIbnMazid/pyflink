9be3d59006e518acada77217e63e52ffe696d44e
sunny-selise <raihan.sunny@selise.ch>
2022-12-06 12:52:52 +0600

    PEP8 boolean operator

--- fifa/operators/video_wipe_detector_function.py
+++ fifa/operators/video_wipe_detector_function.py
@@ -8,7 +8,7 @@ class VideoWipeDetectorFunction(KeyedProcessFunction):
         self.detector = VideoWipeDetector()
 
     def process_element(self, value, ctx: RuntimeContext):
-        if not self.detector.name in value["detectors"]:
+        if self.detector.name not in value["detectors"]:
             yield {"recording_id": value["recording_id"], "frame_index": value["frame_index"]}
         else:
             result = self.detector.run(value)
